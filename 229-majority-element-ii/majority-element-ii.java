d4d8161a5d5d0f4b8a8a41146a5095b12b4a0d3b
Yan Shi <leonshiyan@gmail.com>
2023-10-10 12:23:03 -0700

    Time: 10 ms (50.93%) | Memory: 46.6 MB (52.17%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        Map<Integer, Integer> counts = new HashMap<>();
+        int n = nums.length;
+        int threshold = n / 3;
+
+        for (int num : nums) {
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (entry.getValue() > threshold) {
+                result.add(entry.getKey());
+            }
+        }
+
+        return result;
+    }
+}
